723158b7ffa55a0e02668f0f1aa68db666c9ee77
AmolNimbalkar <animbalkar.1792@gmail.com>
2025-08-15 23:06:28 +0530

    My Payment App: Day 1 task

---
+++ payment-orchestrator/src/test/java/com/payment/payment_orchestrator/PaymentOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.payment_orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
