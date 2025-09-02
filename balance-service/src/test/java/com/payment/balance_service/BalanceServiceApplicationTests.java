a0beefb099e4a4581bdc39ff09a351d9e530374e
AmolNimbalkar <animbalkar.1792@gmail.com>
2025-09-02 19:48:45 +0530

    My Payment App: Day 6 task updates

---
+++ balance-service/src/test/java/com/payment/balance_service/BalanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.balance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
